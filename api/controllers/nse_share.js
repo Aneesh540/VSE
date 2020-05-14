fb357f99050fa25c6ff838c62ffbd43066a38160
Aneesh <aneeshjain203@gmail.com>
2020-05-15 00:37:33 +0530

    API of NSE companies completed

--- api/controllers/nse_share.js
+++ api/controllers/nse_share.js
@@ -15,20 +15,36 @@ const company_details = function(req, res, next){
         if(result){
 
             res.status(201).json({
-                statusCode : "200",
+                statusCode : 200,
                 data : result
             });
         }
 
         else{
-            res.status(404).json({statusCode : "404", message : "Not found"})
+            res.status(404).json({statusCode : 404, message : "Not found"})
         }
     })
     .catch((err)=>{
-        res.status(404).json({statusCode : "500", message : 'error while fetching details'});
+        res.status(404).json({statusCode : 500, message : 'error while fetching details'});
+    });
+}
+
+
+const company_names = function(req, res, next){
+    let name =  req.params.name.replace("%20", " ");
+    // print(name);
+    NSE_COMPANIES.find({name : {$regex :name, $options : 'i'}}).select('-_id -__v')
+    .then( (result) => {
+        // print(result);
+        res.status(200).json({statusCode : "200", data : result });
+    })
+    .catch( (err) => {
+        res.status(404).json({statusCode : "404", message : "error while fetching"});
     });
 }
 
 module.exports = { 
-    company_details : company_details
+    company_details : company_details,
+    company_names : company_names
+
 }
