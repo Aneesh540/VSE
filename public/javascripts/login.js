2859585bec17ac15d5003fa2129101d7085fb886
Aneesh <aneeshjain203@gmail.com>
2020-05-22 23:45:12 +0530

    User portfolio template done

---
+++ public/javascripts/login.js
@@ -0,0 +1,44 @@
+"use strict";
+print = console.log;
+
+
+let link = document.querySelector('#change_form a');
+
+link.addEventListener('click', (e) =>{
+
+    e.preventDefault();
+    let button = document.querySelector('#button');
+
+    if (link.innerHTML === 'Signup'){
+        let cp = document.querySelector('.txt_field');
+
+        let clon = cp.cloneNode(true);
+
+        let input = clon.children[0];
+        let label = clon.children[2];
+
+        input.name = "email";
+        input.type = "email";
+        label.innerHTML = "Email";
+
+        cp.before(clon);
+
+        link.innerHTML = "Login";
+        button.innerHTML = "Signup";
+
+    }
+
+    else{
+
+        let cp = document.querySelector('.txt_field');
+        cp.remove();
+
+        link.innerHTML = "Signup";
+        button.innerHTML = "Login";
+
+    }
+
+
+    
+
+});
--- routes/index.js
+++ routes/index.js
@@ -7,8 +7,13 @@ const api = require('../api/controllers/index');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+
+  let shares = [{code : "LT", price : 800.12, qty : 10},
+  {code : "SBIN", price : 155, qty : 150},
+  {code : "NIFTY", price : 800.12, qty : 10},
+  {code : "HUL", price : 2000, qty : 10}  ];
   
-  res.render('index', { title: 'Express app' });
+  res.render('user', { title: 'Express app', shares : shares });
 });
 
 module.exports = router;
