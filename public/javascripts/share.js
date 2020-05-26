578813bb49b20e5282f15f51ffe6a8552c50165f
Aneesh <aneeshjain203@gmail.com>
2020-05-26 21:40:06 +0530

    Sell share method completed

--- public/javascripts/share.js
+++ public/javascripts/share.js
@@ -2,8 +2,25 @@ print = console.log;
 const endpoint = "http://localhost:3000/api";
 let API_KEY = 'RXGZSQ2O11C8CHSU';
 
+let get_cookie = function(name){
+
+    let cookie_list = document.cookie.split(';');
+    
+    for(let i=0; i<cookie_list.length; i++){
+
+        let COOKIE = cookie_list[i].trim();
+
+        let [key, value] = COOKIE.split('=');
+
+        if(key === name){
+            return value;
+        }
+    }
+}
+
+
 // Example POST method implementation:
-async function postData(url = '', data = {}) {
+async function postData(url, data) {
     // Default options are marked with *
     const response = await fetch(url, {
       method: 'POST', // *GET, POST, PUT, DELETE, etc.
@@ -12,7 +29,7 @@ async function postData(url = '', data = {}) {
       credentials: 'same-origin', // include, *same-origin, omit
       headers: {
         'Content-Type': 'application/json',
-        'Authorization' : 'Bearer ' + document.cookie.split('access_token=')[1]
+        'Authorization' : 'Bearer ' + get_cookie('access_token')
         // 'Content-Type': 'application/x-www-form-urlencoded',
       },
       redirect: 'follow', // manual, *follow, error
@@ -60,7 +77,7 @@ const buy_share = function(){
     let url = endpoint + `/${username}/buy`;
 
     
-    if(confirm(`place order of ${data.quantity} shares`)){
+    if(confirm(`place order of ${data.quantity} shares @ ${data.price} = Rs. ${data.price*data.quantity}`)){
         postData(url, data)
         .then(result => {
             console.log(result);
@@ -73,6 +90,49 @@ const buy_share = function(){
 
 }
 
+const sell_share = function(){
+    let x = document.querySelectorAll('#share_lot_info input');
+    let sell_price = document.querySelector('#current_price').innerHTML;
+    let sell_list = []
+    let Sum = 0;
+    x.forEach( item => {
 
-let x = document.querySelectorAll('#all_list input');
-x.forEach( item => {print(item.id); print(item.value)});
+        if ( parseInt(item.value) > 0){
+            sell_list.push({id : item.id, 
+                            quantity : item.value,
+                            price : sell_price});
+            Sum += parseInt(item.value)
+        }
+    
+    });
+
+    if(sell_list.length === 0){
+        alert('Select atleast 1');
+        return
+    }
+
+    if(confirm(`Place order shares ${Sum} @ ${sell_price} = Rs. ${Sum*sell_price}`)){
+        let username = document.getElementById('username').value.trim();
+        let url = endpoint + `/${username}/sell`;
+        let data = sell_list;
+        postData(url, data)
+        .then(result => {
+            console.log(result);
+            print(result.credit_amount);
+
+            if(result.data.credit_amount === 0){
+                alert('Cannot sell, quantity more than available');
+            }
+            alert(`transaction completed quantity sold:${Sum}, price:${sell_price}`);
+
+
+        })
+        .catch(err => alert('Not able to complete transaction'));
+    }
+}
+    
+    
+
+
+
+
